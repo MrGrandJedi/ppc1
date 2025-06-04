6bb385d855a66239a1c06c2ddebd8fa846e62a46
TheRandomerss <150558634+TheRandomerss@users.noreply.github.com>
2025-06-04 21:19:27 +0100

    Update traffic.js

--- traffic.js
+++ traffic.js
@@ -22,20 +22,7 @@ const weightedLocations = {
   ca: 3,
   us: 30,
   uk: 5,
-  de: 1,
-  jp: 1,
-  sg: 1,
-  kr: 1,
-  it: 1,
-  es: 1,
-  in: 1,
-  id: 1,
-  ph: 1,
-  th: 1,
-  tr: 1,
-  mx: 1,
-  no: 1,
-  hk: 1,
+ dk : 20,
 };
 
 // Build weighted list
